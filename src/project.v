0c2090ce7ef9c003b3d959e55a4436400dbee7b7
Mike Bell <mdb036@gmail.com>
2024-04-20 22:26:59 +0100

    Super tiny

--- src/project.v
+++ src/project.v
@@ -1,24 +1,136 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Michael Bell
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_MichaelBell_tinyQV (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
+/*verilator lint_off UNUSEDSIGNAL*/
+    input  wire [7:0] uio_in,   // IOs: Input path - only some bits used
+/*verilator lint_on UNUSEDSIGNAL*/
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+/*verilator lint_off UNUSEDSIGNAL*/
+    input  wire       ena,
+/*verilator lint_on UNUSEDSIGNAL*/
+    input  wire       clk,
+    input  wire       rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // Address to peripheral map
+    localparam PERI_NONE = 3'h7;
+    localparam PERI_GPIO_OUT = 3'h0;
+    localparam PERI_GPIO_IN = 3'h1;
+    localparam PERI_UART = 3'h4;
+    localparam PERI_UART_STATUS = 3'h5;
+
+    // Register the reset on the negative edge of clock for safety.
+    // This also allows the option of async reset in the design, which might be preferable in some cases
+    /* verilator lint_off SYNCASYNCNET */
+    reg rst_reg_n;
+    /* verilator lint_on SYNCASYNCNET */
+    always @(negedge clk) rst_reg_n <= rst_n;
+
+    // Bidirs are used for SPI interface
+    wire [3:0] qspi_data_in = {uio_in[5:4], uio_in[2:1]};
+    wire [3:0] qspi_data_out;
+    wire [3:0] qspi_data_oe;
+    wire       qspi_clk_out;
+    wire       qspi_flash_select;
+    wire       qspi_ram_a_select;
+    wire       qspi_ram_b_select;
+    assign uio_out = {qspi_ram_b_select, qspi_ram_a_select, qspi_data_out[3:2], 
+                      qspi_clk_out, qspi_data_out[1:0], qspi_flash_select};
+    assign uio_oe = rst_n ? {2'b11, qspi_data_oe[3:2], 1'b1, qspi_data_oe[1:0], 1'b1} : 8'h00;
+
+    wire [27:0] addr;
+    wire  [1:0] write_n;
+/*verilator lint_off UNUSEDSIGNAL*/
+    wire  [1:0] read_n;         // Always assumed to be whole word
+    wire [31:0] data_to_write;  // Currently only bottom byte used.
+/*verilator lint_on UNUSEDSIGNAL*/
+
+    wire        data_ready;
+    reg [31:0] data_from_read;
+
+    wire       uart_txd;
+    reg  [7:1] gpio_out;
+
+    // All transactions to peripherals complete immediately
+    assign data_ready = 1'b1;
+    reg [2:0] connect_peripheral;
+
+    // UART
+    wire uart_tx_busy;
+    wire uart_tx_start = write_n != 2'b11 && connect_peripheral == PERI_UART;
+
+    tinyQV i_tinyqv(
+        .clk(clk),
+        .rstn(rst_reg_n),
+
+        .data_addr(addr),
+        .data_write_n(write_n),
+        .data_read_n(read_n),
+        .data_out(data_to_write),
+
+        .data_ready(data_ready),
+        .data_in(data_from_read),
+
+        .spi_data_in(qspi_data_in),
+        .spi_data_out(qspi_data_out),
+        .spi_data_oe(qspi_data_oe),
+        .spi_clk_out(qspi_clk_out),
+        .spi_flash_select(qspi_flash_select),
+        .spi_ram_a_select(qspi_ram_a_select),
+        .spi_ram_b_select(qspi_ram_b_select)
+    );
+
+    assign uo_out[0] = uart_txd;
+    assign uo_out[1] = gpio_out[1];
+    assign uo_out[2] = gpio_out[2];
+    assign uo_out[3] = gpio_out[3];
+    assign uo_out[4] = gpio_out[4];
+    assign uo_out[5] = gpio_out[5];
+    assign uo_out[6] = gpio_out[6];
+    assign uo_out[7] = gpio_out[7];
+
+    always @(*) begin
+        if ({addr[27:6], addr[1:0]} == 24'h800000) 
+            connect_peripheral = addr[4:2];
+        else
+            connect_peripheral = PERI_NONE;
+    end
+
+    // Read data
+    always @(*) begin
+        case (connect_peripheral)
+            PERI_GPIO_OUT:    data_from_read = {24'h0, uo_out};
+            PERI_GPIO_IN:     data_from_read = {24'h0, ui_in};
+            PERI_UART_STATUS: data_from_read = {31'h0, uart_tx_busy};
+            default:          data_from_read = 32'h0;
+        endcase
+    end
+
+    // GPIO Out
+    always @(posedge clk) begin
+        if (!rst_reg_n) begin
+            gpio_out <= 0;
+        end
+        if (write_n != 2'b11) begin
+            if (connect_peripheral == PERI_GPIO_OUT) gpio_out <= data_to_write[7:1];
+        end
+    end
+
+    uart_tx #(.CLK_HZ(64_000_000), .BIT_RATE(4_000_000)) i_debug_uart_tx(
+        .clk(clk),
+        .resetn(rst_reg_n),
+        .uart_txd(uart_txd),
+        .uart_tx_en(uart_tx_start),
+        .uart_tx_data(data_to_write[7:0]),
+        .uart_tx_busy(uart_tx_busy) 
+    );
 
 endmodule
